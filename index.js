44fb082cd67dc27ae5c25dbbb7f111bda98d09c4
shoota <shoota.dev@gmail.com>
2014-05-09 15:57:28 +0900

    findbyId

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports.mow = {
+'use strict'
 
+var client = require('../lib/client');
+var util = require('../lib/util');
+
+// constructor
+var mow = function(vendor){
+	if(! vendor) return undefined;
+	this.client = new client(vendor):
 };
+
+module.exports = mow;
+
+
+mow.prototype.findById = function(id, callback){
+	this.client.find({id:id}, callback);
+}
